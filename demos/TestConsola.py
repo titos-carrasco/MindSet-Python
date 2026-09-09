2e8e44d1fb6104a8e58f533970aa2d1dcf19f510
Roberto Carrasco <titos.carrasco@gmail.com>
2026-09-09 16:27:01 -0300

    Ajusta ejecución para finalizarla con CTRlL-C

--- demos/TestConsola.py
+++ demos/TestConsola.py
@@ -1,27 +1,24 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from __future__ import print_function
 import time
-
 from mindset.MindSet import *
 
-class TestConsola():
-    def __init__( self ):
-        pass
 
-    def run( self ):
+class TestConsola:
+    def __init__(self, port):
         # Bluetooth version
         #   headSet = MindSet( '/dev/rfcomm4' )
-        # RF version: 0x0000=connect any, 0xXXYY=connect with  0xXXY
+        # RF version: 0x0000=connect any, 0xXXYY=connect with  0xXXYY
         #   headSet = MindSet( '/dev/ttyUSB0', 0x0000 )
+        self.headSet = MindSet(port)
+
+    def run(self):
         msd = MindSetData()
-        headSet = MindSet( '/dev/rfcomm4' )
-        if( headSet.connect() ):
-            t = time.time()
-            while( time.time() - t < 20  ):
-                headSet.getMindSetData( msd )
-                print( '%d %d %d %d %d %u %u %u %u %u %u %u %u' % (
+        if self.headSet.connect():
+            while True:
+                try:
+                    self.headSet.getMindSetData(msd)
+                    print(
+                        "%d %d %d %d %d %u %u %u %u %u %u %u %u"
+                        % (
                             msd.poorSignalQuality,
                             msd.attentionESense,
                             msd.meditationESense,
@@ -34,11 +31,16 @@ class TestConsola():
                             msd.lowBeta,
                             msd.highBeta,
                             msd.lowGamma,
-                            msd.midGamma
-                        ), flush=True )
-                time.sleep( 0.001 )
-            headSet.disconnect()
+                            msd.midGamma,
+                        ),
+                        flush=True,
+                    )
+                    time.sleep(0.001)
+                except KeyboardInterrupt:
+                    break
+            self.headSet.disconnect()
 
 
-if( __name__ == "__main__" ):
-    TestConsola().run()
+# -- show time
+app = TestConsola("COM21")
+app.run()
